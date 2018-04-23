46ff58c1a1ee22f5b0b9e7a60fbef57007631d78
kallaballa <amir@viel-zu.org>
2018-04-23 17:02:00 +0200

    initial

--- src/libnfpmk.hpp
+++ src/libnfpmk.hpp
@@ -54,6 +54,10 @@ public:
 	coord_t x_;
 	coord_t y_;
 
+	bool operator==(const point_t& other) const {
+		return this->x_ == other.x_ && this->y_ == other.y_;
+	}
+
 	bool operator<(const point_t&  other) const {
       return  this->x_ < other.x_ || (this->x_ == other.x_ && this->y_ < other.y_);
   }
@@ -67,48 +71,109 @@ BOOST_GEOMETRY_REGISTER_POINT_2D(libnfpmk::point_t, libnfpmk::coord_t, cs::carte
 
 
 namespace libnfpmk {
-	typedef bg::model::segment<point_t> segment_t;
+
 typedef bg::model::polygon<point_t, false, true> polygon_t;
 typedef std::vector<polygon_t::ring_type> nfp_t;
 typedef bg::model::linestring<point_t> linestring_t;
 
 typedef typename polygon_t::ring_type::size_type psize_t;
 
-void write_svg(std::string const& filename,	typename std::vector<polygon_t> const& polygons, const nfp_t& nfp) {
-	polygon_t nfppoly;
-	for (const auto& pt : nfp.front()) {
-		nfppoly.outer().push_back(pt);
+class segment_t: public bg::model::segment<point_t> {
+public:
+	psize_t index_ = 0;
+	coord_t slope_ = 0;
+	inline segment_t() {
 	}
 
-	for (size_t i = 1; i < nfp.size(); ++i) {
-		nfppoly.inners().push_back({});
-		for (const auto& pt : nfp[i]) {
-			nfppoly.inners().back().push_back(pt);
+	inline segment_t(point_t const& p1, point_t const& p2) :
+			bg::model::segment<point_t>(p1, p2) {
+	}
+
+	bool operator<(const segment_t& other) const {
+		return this->slope_ < other.slope_;
+	}
+};
+
+bg::model::segment<point_t> toBgSegment(segment_t seg) {
+	return *dynamic_cast<bg::model::segment<point_t>*>(&seg);
+}
+
+std::ostream& operator<<(std::ostream& os, const point_t& pt) {
+	os << "{" << pt.x_ << ", " << pt.y_ << "}";
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const segment_t& seg) {
+	os << seg.index_ << '/' << seg.slope_ << ": {" << seg.first << ", " << seg.second << "}";
+	return os;
+}
+
+//an edge group sorted by slope of segments
+class EdgeGroup : public std::set<segment_t> {
+public:
+	bool positive_;
+
+	EdgeGroup(bool positive = true) : std::set<segment_t>(), positive_ (positive) {
+	}
+};
+
+void write_svg(std::string const& filename,	typename std::vector<polygon_t> const& polygons, const nfp_t& nfp) {
+	std::ofstream svg(filename.c_str());
+	boost::geometry::svg_mapper<point_t> mapper(svg, 100, 100,	"width=\"200mm\" height=\"200mm\" viewBox=\"-250 -250 500 500\"");
+
+	if(!nfp.empty()) {
+		polygon_t nfppoly;
+		for (const auto& pt : nfp.front()) {
+			nfppoly.outer().push_back(pt);
+		}
+
+		for (size_t i = 1; i < nfp.size(); ++i) {
+			nfppoly.inners().push_back({});
+			for (const auto& pt : nfp[i]) {
+				nfppoly.inners().back().push_back(pt);
+			}
+		}
+		bg::correct(nfppoly);
+		mapper.add(nfppoly);
+		mapper.map(nfppoly, "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
+
+		for(auto& r: nfppoly.inners()) {
+			if(r.size() == 1) {
+				mapper.add(r.front());
+				mapper.map(r.front(), "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
+			} else if(r.size() == 2) {
+				segment_t seg(r.front(), *(r.begin()+1));
+				mapper.add(toBgSegment(seg));
+				mapper.map(toBgSegment(seg), "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
+			}
 		}
 	}
-	std::ofstream svg(filename.c_str());
-
-	boost::geometry::svg_mapper<point_t> mapper(svg, 100, 100,	"width=\"200mm\" height=\"200mm\" viewBox=\"-250 -250 500 500\"");
 	for (auto p : polygons) {
 		mapper.add(p);
 		mapper.map(p, "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
 	}
-	bg::correct(nfppoly);
-	mapper.add(nfppoly);
-	mapper.map(nfppoly, "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
+}
 
-	for(auto& r: nfppoly.inners()) {
-		if(r.size() == 1) {
-			mapper.add(r.front());
-			mapper.map(r.front(), "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
-		} else if(r.size() == 2) {
-			segment_t seg(r.front(), *(r.begin()+1));
-			mapper.add(seg);
-			mapper.map(seg, "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(204,153,0);stroke-width:2");
+void write_svg(std::string const& filename,	std::vector<EdgeGroup> const& edgeGroups) {
+	std::ofstream svg(filename.c_str());
+	boost::geometry::svg_mapper<point_t> mapper(svg, 100, 100,	"width=\"200mm\" height=\"200mm\" viewBox=\"-250 -250 1000 1000\"");
+
+
+	std::string colorPos = "255,0,0";
+	std::string colorNeg = "0,255,0";
+	std::string color;
+	for (auto g : edgeGroups) {
+		if(g.positive_)
+			color = colorPos;
+		else
+			color = colorNeg;
+
+		for(auto seg: g) {
+			mapper.add(toBgSegment(seg));
+			mapper.map(toBgSegment(seg), "fill-opacity:0.5;fill:rgb(" + color + ");stroke:rgb(" + color + ");stroke-width:2");
 		}
 	}
 }
-
 
 enum Alignment {
 	LEFT,
@@ -130,7 +195,8 @@ Alignment get_alignment(const segment_t& seg, const point_t& pt){
 }
 point_t normalize(const point_t& pt) {
 	point_t norm = pt;
-	coord_t len = bg::length(segment_t{{0,0},pt});
+	segment_t seg{{0,0},pt};
+	coord_t len = bg::length(*dynamic_cast<bg::model::segment<point_t>*>(&seg));
 
 	if(len == 0.0L)
 		return {0,0};
@@ -187,6 +253,95 @@ void removeCoLinear(polygon_t& p) {
 		removeCoLinear(r);
 }
 
+polygon_t::ring_type invert(const polygon_t::ring_type& r) {
+	polygon_t::ring_type ri;
+	for(point_t pt : r) {
+		ri.push_back(point_t(-pt.x_, -pt.y_));
+	}
+
+	return ri;
+}
+
+point_t find_minimum_y(const polygon_t::ring_type& r) {
+	std::vector<psize_t> result;
+	coord_t min = MAX_COORD;
+
+	for(psize_t i = 0; i < r.size() - 1; ++i) {
+		if(r[i].y_< min) {
+			result.clear();
+			min = r[i].y_;
+			result.push_back(i);
+		} else if (r[i].y_ == min) {
+			result.push_back(i);
+		}
+	}
+	return r[result.front()];
+}
+
+std::vector<segment_t> makeEdges(polygon_t::ring_type r, const point_t& lowest) {
+	r.erase(r.end() - 1);
+	psize_t first = 0;
+	for(size_t i = 0; i < r.size(); ++i) {
+		if(r[i] == lowest) {
+			first = i;
+			break;
+		}
+	}
+
+	std::vector<segment_t> edges;
+	for(size_t i = first + 1; i < (r.size() + first + 1); ++i) {
+		if(i > r.size() - 1) {
+			if(i == r.size()) {
+				edges.push_back(segment_t(r.back(), r.front()));
+			} else {
+				edges.push_back(segment_t(r[i - r.size() - 1], r[i - r.size()]));
+			}
+		} else {
+			if(i == 0) {
+				edges.push_back(segment_t(r.back(), r.front()));
+			} else {
+				edges.push_back(segment_t(r[i - 1], r[i]));
+			}
+		}
+	}
+	return edges;
+}
+
+void labelEdges(std::vector<segment_t>& edges, const point_t& lowest) {
+	psize_t first = 0;
+	for(size_t i = 0; i < edges.size(); ++i) {
+		if(edges[i].first == lowest) {
+			first = i;
+			break;
+		}
+	}
+
+	size_t count = 0;
+	for(size_t i = first; i < (edges.size() + first); ++i) {
+		if(i > edges.size() - 1) {
+			edges[i - edges.size()].index_ = count++;
+		} else {
+			edges[i].index_ = count++;
+		}
+	}
+}
+
+coord_t calculateSlope(const segment_t& seg) {
+  coord_t d_x = seg.second.x_ - seg.first.x_;
+  coord_t d_y = seg.second.y_ - seg.first.y_;
+  return (M_PI - atan2(d_y, -d_x)) / (2 * M_PI) * 360;
+}
+
+void calculateSlopes(std::vector<segment_t>& edges) {
+	for(segment_t& seg: edges) {
+		seg.slope_ = calculateSlope(seg);
+	}
+}
+
+bool hasSameSign(coord_t x, coord_t y) {
+    return (x == 0 && y == 0) || (x > 0 && y > 0) || (x < 0 && y < 0);
+}
+
 nfp_t generateNFP(polygon_t& pA, polygon_t& pB, const bool checkValidity = true) {
 	removeCoLinear(pA);
 	removeCoLinear(pB);
@@ -200,6 +355,115 @@ nfp_t generateNFP(polygon_t& pA, polygon_t& pB, const bool checkValidity = true)
 			throw std::runtime_error("Polygon B is invalid: " + reason);
 	}
 
+	polygon_t::ring_type rBCW;
+	rBCW.resize(pB.outer().size());
+	//make clockwise
+	std::reverse_copy(pB.outer().begin(), pB.outer().end(), rBCW.begin());
+	//invert coordinates
+	polygon_t::ring_type rBCWi = invert(rBCW);
+	polygon_t::ring_type rA = pA.outer();
+	point_t ptMinYA = find_minimum_y(rA);
+	point_t ptMinYBi = find_minimum_y(rBCWi);
+
+	std::vector<segment_t> edgesA = makeEdges(rA, ptMinYA);
+	std::vector<segment_t> edgesBi = makeEdges(rBCWi, ptMinYBi);
+	std::cerr << "label" << std::endl;
+
+	segment_t seg1{{0,0},{1,0}};
+	segment_t seg2{{0,0},{0,1}};
+	segment_t seg3{{0,0},{-1,0}};
+	segment_t seg4{{0,0},{0,-1}};
+	std::cerr << calculateSlope(seg1) << std::endl;
+	std::cerr << calculateSlope(seg2) << std::endl;
+	std::cerr << calculateSlope(seg3) << std::endl;
+	std::cerr << calculateSlope(seg4) << std::endl;
+	labelEdges(edgesA, ptMinYA);
+	labelEdges(edgesBi, ptMinYBi);
+	calculateSlopes(edgesA);
+	calculateSlopes(edgesBi);
+
+	for(const segment_t& seg: edgesA) {
+		std::cerr << seg << std::endl;
+	}
+	std::cerr << std::endl;
+
+	for(const segment_t& seg: edgesBi) {
+		std::cerr << seg << std::endl;
+	}
+
+	std::vector<coord_t> slopDiff;
+	coord_t sd;
+	for(size_t i = 0; i < edgesBi.size(); ++i) {
+		if(i == 0) {
+			sd = edgesBi.front().slope_ - edgesBi.back().slope_;
+		} else {
+			sd = edgesBi[i].slope_ - edgesBi[i - 1].slope_;
+		}
+
+		if(sd > 180)
+			sd -= 360;
+		else if(sd < -180)
+			sd += 360;
+
+		slopDiff.push_back(sd);
+	}
+
+	std::vector<size_t> turningPoint;
+
+	for(size_t i = 0; i < slopDiff.size(); ++i) {
+		if(i == 0) {
+			if(!hasSameSign(slopDiff.back(), slopDiff[0])) {
+				turningPoint.push_back(i);
+			}
+		} else {
+			if(!hasSameSign(slopDiff[i], slopDiff[i - 1])) {
+				turningPoint.push_back(i);
+			}
+		}
+	}
+
+	std::vector<EdgeGroup> edgeGroups;
+	bool positive = false;
+	if(!turningPoint.empty()) {
+		assert(turningPoint.size() > 1);
+		for(size_t i = 1; i < turningPoint.size(); ++i) {
+			positive = !positive;
+			edgeGroups.push_back({positive});
+			for(size_t j = turningPoint[i - 1]; j < turningPoint[i]; ++j) {
+				edgeGroups.back().insert(edgesBi[j]);
+			}
+		}
+		edgeGroups.push_back({false});
+		if(turningPoint.back() <= (edgesBi.size() - 1)) {
+			for(size_t i = turningPoint.back(); i < edgesBi.size(); ++i) {
+				edgeGroups.back().insert(edgesBi[i]);
+			}
+		}
+
+		if(turningPoint.front() > 0) {
+			for(size_t i = 0; i < turningPoint.front(); ++i) {
+				edgeGroups.back().insert(edgesBi[i]);
+			}
+		}
+
+		if(edgeGroups.back().empty())
+			edgeGroups.erase(edgeGroups.end() - 1);
+	} else {
+		EdgeGroup eg;
+		for(auto e : edgesBi)
+			eg.insert(e);
+		edgeGroups.push_back(eg);
+	}
+
+	std::cerr << "num edgegroups: " << edgeGroups.size() << std::endl;
+	#ifdef NFP_DEBUG
+  write_svg("edgeGroups.svg", edgeGroups);
+#endif
+
+	std::cerr << "turning point"  << std::endl;
+	for(const size_t& tp : turningPoint) {
+		std::cerr << tp << std::endl;
+	}
 	nfp_t nfp;
 
 
